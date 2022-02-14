d2701a1a106eda6030c233ba7524c33c51e98de7
Jacky Lui <jackymlui@live.com>
2022-02-14 17:29:45 +1000

    changed back to img

--- components/header.js
+++ components/header.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import Image from "next/image";
 
 class Header extends Component {
   render() {
     return (
       <div className="px-2 bg-white" style={{ position: "sticky", top: "0px" }}>
-        <Image src="/logo.png" alt="Picture of Logo" height={60} width={160} />
+        <img src="/logo.png" alt="Picture of Logo" height={60} width={160} />
       </div>
     );
   }
