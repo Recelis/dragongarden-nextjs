5ab09ef3730e09ceebc3786650befa8022b49175
Jacky Lui <jackymlui@live.com>
2022-02-14 16:01:44 +1000

    more apostrophe fixes

--- components/dineinmenu/childrens.js
+++ components/dineinmenu/childrens.js
@@ -4,7 +4,7 @@ import MenuItem from "./menuItem";
 const Childrens = () => {
   return (
     <div className="menu-category">
-      <h3 className="menu-subheading">Children's Menu 儿童菜单</h3>
+      <h3 className="menu-subheading">Children&apos;s Menu 儿童菜单</h3>
       <MenuItem itemNumber="86" itemName="Chicken Nuggets and Chips" itemPrice="10.50" />
       <MenuItem itemNumber="87" itemName="Fish and Chips (crumbed or battered)" itemPrice="10.50" />
       <MenuItem
