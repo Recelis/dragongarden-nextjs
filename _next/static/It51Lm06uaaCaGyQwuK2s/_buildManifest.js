7d898bf34f6a3a6b535071b3adc24b3196284403
Jacky Lui <jackymlui@live.com>
2022-02-14 17:29:45 +1000

    changed back to img

--- _next/static/It51Lm06uaaCaGyQwuK2s/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/487-1a59c7a3b54e1192.js","static/chunks/pages/index-a6066b9458c28ed0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
