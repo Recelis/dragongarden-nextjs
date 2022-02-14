089333697e270da62f5d9e0f90fc0c9abdcf9edb
Jacky Lui <jackymlui@live.com>
2022-02-14 16:47:55 +1000

    setting up image optimisation

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     return (
       <div className="px-2 bg-white" style={{ position: "sticky", top: "0px" }}>
-        <Image src={Logo} alt="Picture of Logo" height={60} width={160} />
+        <Image src={require("./images/logo.png")} alt="Picture of Logo" height={60} width={160} />
       </div>
     );
   }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const debug = process.env.NODE_ENV !== "production";
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = {
-  reactStrictMode: true,
-  assetPrefix: !debug ? "/dragongarden-nextjs/" : "",
-};
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      reactStrictMode: true,
+      assetPrefix: !debug ? "/dragongarden-nextjs/" : "",
+    },
+  ],
+]);
