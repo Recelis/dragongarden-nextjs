253f7ef18c5c8bd2e67e8c95f8ab75b9793ba21a
Jacky Lui <jackymlui@live.com>
2022-02-14 17:24:19 +1000

    moved images to public from own folder

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div className="px-2 bg-white" style={{ position: "sticky", top: "0px" }}>
-        <img src={"/images/logo.png"} alt="Picture of Logo" height={60} width={160} />
+        <img src="/logo.png" alt="Picture of Logo" height={60} width={160} />
       </div>
     );
   }
